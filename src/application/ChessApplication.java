28416ce1327832862e9512b4906fd648f88b9020
Breno Cunha <brenocostadacunha@gmail.com>
2023-08-18 02:15:43 -0300

    Moving Pieces

--- src/application/ChessApplication.java
+++ src/application/ChessApplication.java
@@ -1,14 +1,31 @@
 package application;
 
-import boardgame.Position;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class ChessApplication {
     public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UiBoard uiBoard = new UiBoard();
-        uiBoard.printBoard(chessMatch.getPieces());
-
-
+        
+        
+        while(true) {
+        	UiBoard.printBoard(chessMatch.getPieces());
+        	System.out.println();
+        	System.out.print("Source: ");
+        	ChessPosition source = UiBoard.readChessPosition(sc);
+        	
+        	System.out.println();
+        	System.out.print("Target: ");
+        	ChessPosition target = UiBoard.readChessPosition(sc);
+        	
+        	ChessPiece capturedPiece = chessMatch.performChessMovie(source, target);
+        }
+         
+         
+        
     }
 }
