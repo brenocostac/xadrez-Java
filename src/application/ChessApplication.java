de399b3b3e358dca3ebcf18482efa345f05c096d
Breno Cunha <brenocostadacunha@gmail.com>
2023-08-20 21:25:50 -0300

    Exception , movimentos genericos e limpar a tela

--- src/application/ChessApplication.java
+++ src/application/ChessApplication.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -13,16 +15,28 @@ public class ChessApplication {
         
         
         while(true) {
-        	UiBoard.printBoard(chessMatch.getPieces());
-        	System.out.println();
-        	System.out.print("Source: ");
-        	ChessPosition source = UiBoard.readChessPosition(sc);
-        	
-        	System.out.println();
-        	System.out.print("Target: ");
-        	ChessPosition target = UiBoard.readChessPosition(sc);
-        	
-        	ChessPiece capturedPiece = chessMatch.performChessMovie(source, target);
+        	try {
+	        	UiBoard.clearScreen();
+	        	UiBoard.printBoard(chessMatch.getPieces());
+	        	System.out.println();
+	        	System.out.print("Source: ");
+	        	ChessPosition source = UiBoard.readChessPosition(sc);
+	        	
+	        	System.out.println();
+	        	System.out.print("Target: ");
+	        	ChessPosition target = UiBoard.readChessPosition(sc);
+	        	
+	        	ChessPiece capturedPiece = chessMatch.performChessMovie(source, target);
+        	}
+        	catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+        	catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+        	 
         }
          
          
